10b2045d61902f5536b39b430aad7bc57e2c2c93
ABDE ALI MEWA WALA <ali_abde37@outlook.com>
2026-01-31 13:26:00 -0500

    updates

--- app/core/security.py
+++ app/core/security.py
@@ -1,5 +1,5 @@
 from datetime import datetime, timezone, timedelta
-from jose import jwt, JWTError
+from jose import JWTError, jwe
 from app.core.config import settings
 
 
@@ -7,15 +7,16 @@ def create_token(data:dict, expire_minutes=30):
     to_encode=data.copy()
     expire=datetime.now(timezone.utc) + timedelta(minutes=expire_minutes)
     to_encode.update({'exp':expire})
-    return jwt.encode(
+    return jwe.encrypt(
         to_encode, settings.JWT_SECRET_KEY, algorithm=settings.JWT_ALGORITHM
     )
     
 def verify_token(token:str):
     try:
-        payload=jwt.decode(
+        payload=jwe.decrypt(
             token, settings.JWT_SECRET_KEY, algorithms=[settings.JWT_ALGORITHM]
         )
+        
         return payload
     except JWTError:
         return None
